ebddf7ca45b20558c2e596e5237e748492ed8f2b
Gyeonghyeon1 <to5289@naver.com>
2025-06-27 22:34:56 +0900

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 여기에 너의 시트 CSV 링크 붙여넣기!
-const sheetUrl = "https://docs.google.com/spreadsheets/d/1A2B3C4D5E6F7G8H9I0J/export?format=csv";
+const sheetUrl = "https://docs.google.com/spreadsheets/d/1jTOurtDad_WpwH7bXLqxPcxRvgN-0bhzqMRajfqD9Zw/export?format=csv";
 
 fetch(sheetUrl)
   .then(res => res.text())
